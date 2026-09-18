d96662fc29b3a663928f2998986c4879f60355ea
2411140006_Arita <2411140006v@ed.fuk.kindai.ac.jp>
2026-09-18 04:31:27 +0000

    パッチノート1.0.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_18_040900) do
+  create_table "books", force: :cascade do |t|
+    t.string "author", null: false
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.date "published_on"
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+  end
 end
